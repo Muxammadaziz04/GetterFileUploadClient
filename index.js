babf54ad30e61acee5cfb145f246c148be13218a
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-03-09 10:11:46 +0500

    Export file name changet

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import uploadImage from './src/uploadImage.js'
 import uploadVideo from './src/uploadVideo.js'
 import removeFile from './src/removeFile.js'
 
-class GetterFileUpload1 {
+class GetterFileUpload {
     constructor(url) {
         this.url = url
         this.uploadImage = uploadImage
@@ -11,4 +11,4 @@ class GetterFileUpload1 {
     }
 }
 
-export default GetterFileUpload1
+export default GetterFileUpload
